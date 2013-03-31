197b23ec5c3d2a6c22767648f2f9dda755d64126
leo-bogert <github@leo.bogert.de>
2013-04-01 01:08:07 +0200

    Add lib-bash-leo.sh which contains shell options and functions which I use in many scripts. Future commits will update those scripts to source this lib instead of using copypasta of that stuff.

---
+++ lib-bash-leo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -o nounset
+set -o pipefail
+#set -o errexit	# see enable_errexit_and_errtrace
+#set -o errtrace # see enable_errexit_and_errtrace
+shopt -s nullglob
+shopt -s failglob
+
+secho() {
+	printf '%s\n' "$*"
+}
+
+stdout() {
+	secho "$@" >&1
+}
+
+stderr() {
+	secho "$@" >&2
+}
+
+die() {
+	stderr "$@"
+	exit 1
+}
+
+err_handler() {
+	die "error at line $1, last exit code is $2"
+}
+
+enable_errexit_and_errtrace() {
+	set -o errexit
+	set -o errtrace
+	trap 'err_handler "$LINENO" "$?"' ERR
+}
+
+remove_trailing_slash_on_path() {
+	if [[ "$1" != '/' ]] ; then
+		secho "${1%/}"
+	else
+		secho "$1"
+	fi
+}
+
+enable_errexit_and_errtrace
